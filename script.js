dfe72214fa35fee7d8ee0bb27d813552e7f5aed0
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2024-01-30 03:24:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 //your code here
 let userData;
-fetch("https://randomuser.me/api/").then((res)=>res.json()).then((val)=>
-	{
-	userData= val.data.results.gender;
-	console.log(val.data);
-	document.getElementByClass(".userDetails").innerHTML= userData;	
-	}																
-);
+let userDetails = document.getElementById("userDetails");
+let userImg = document.getElementById("userimg");
+
+let user = document.getElementById("user");
+const apiCall = () => {
+    userDetails.innerHTML = "";
+    fetch("https://randomuser.me/api/").then((res) => res.json()).then((val) => {
+        userData = val.results[0];
+        console.log(userData);
+        console.log(userDetails)
+        userImg.src = userData.picture.thumbnail
+        user.innerHTML = `${userData.name.title} ${userData.name.first} ${userData.name.last}`
+        console.log(userDetails)
+    }
+    );
+}
+apiCall();
+const handleAge = () => {
+    userDetails.innerHTML = userData.dob.age;
+}
+const handleEmail = () => {
+    userDetails.innerHTML = userData.email;
+}
+const handlePhone = () => {
+    userDetails.innerHTML = userData.phone;
+}
+document.getElementById("getUser").addEventListener("click", () => {
+    apiCall();
+})

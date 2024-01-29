a836b1aa44658fae56168e099bb55408d5f5d5a1
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2024-01-29 17:29:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+let userData;
+fetch("https://randomuser.me/api/").then((res)=>res.json()).then((val)=>
+	{
+	userData= val.data.results.gender;
+	console.log(val.data);
+	document.getElementByClass(".userDetails").innerHTML= userData;	
+	}																
+);
